9e6446de3433be67c2a23b9461d1ae7da04d3369
David Whittington <djwhitt@gmail.com>
2020-09-07 10:16:47 -0500

    [spacemacs] Use ~/Org/notes as path for org-brain and deft

--- spacemacs.d/layers/djw-org/layers.el
+++ spacemacs.d/layers/djw-org/layers.el
@@ -4,15 +4,18 @@
        org-enable-reveal-js-support t
        org-want-todo-bindings t
 
-       ;; Journal
-       org-enable-org-journal-support t
-       org-journal-dir "~/Org/journal/"
-
        ;; Archive
        org-archive-location "~/Org/archive.org::* Notes"
-       org-archive-reversed-order t))
+       org-archive-reversed-order t
+
+       ;; Brain
+       org-brain-path "~/Org/notes/"
+
+       ;; Journal
+       org-enable-org-journal-support t
+       org-journal-dir "~/Org/journal/"))
 
 (configuration-layer/declare-layer
  '(deft :variables
-    deft-directory "~/Org/deft/"
+    deft-directory "~/Org/notes/"
     deft-zetteldeft t))
--- spacemacs.d/layers/djw-org/packages.el
+++ spacemacs.d/layers/djw-org/packages.el
@@ -1,9 +1,1 @@
 (setq djw-org-packages '())
-
-(defun org/post-init-org ()
-  (use-package org
-    :commands (evil-org-mode evil-org-recompute-clocks)
-    :init (add-hook 'org-mode-hook 'evil-org-mode)
-    :config
-    (progn
-      (setq org-brain-path "~/Org/brain"))))
