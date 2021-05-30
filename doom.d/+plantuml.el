8f5a30d52f648f720094bab0d56bdf147dfab43f
David Whittington <djwhitt@gmail.com>
2021-05-30 14:38:42 -0500

    [doom] Set PlantUML paths correctly for Nix

--- doom.d/+plantuml.el
+++ doom.d/+plantuml.el
@@ -2,8 +2,18 @@
 
 (after! plantuml-mode
 
-  (let ((plantuml-config (concat (getenv "HOME")  "/.doom.d/+plantuml/config.cfg")))
-    (setq plantuml-executable-args (list "-headless" "-config" plantuml-config)
+  (let* ((config (concat (getenv "HOME")  "/.doom.d/+plantuml/config.cfg"))
+         (executable (locate-file "plantuml" exec-path))
+         (jar (thread-first executable
+                (file-truename)
+                (file-name-directory)
+                (concat "../lib/plantuml.jar")
+                (file-truename))))
+    (setq flycheck-plantuml-executable executable
+          org-plantuml-jar-path jar
+          plantuml-executable-args (list "-headless" "-config" config)
+          plantuml-executable-path executable
+          plantuml-jar-path jar
           plantuml-output-type "png"))
 
   )
