b32b5557faf4949568b1e784c6b97b4c301a0b70
David Whittington <djwhitt@gmail.com>
2022-04-09 13:37:15 -0500

    [fish] Remove npm config (now in xprofile)

--- config/fish/conf.d/npm.fish
+++
@@ -1,5 +0,0 @@
-set -l npm_packages "$HOME/.npm-packages"
-if test -d $npm_packages
-  set -gx NPM_PACKAGES $npm_packages
-  contains "$npm_packages/bin" $fish_user_paths; or set -Ua fish_user_paths "$npm_packages/bin"
-end
