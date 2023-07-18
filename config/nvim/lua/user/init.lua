3bef4da5de8f453c37cd3ebfbed67fdb764a9107
David Whittington <djwhitt@gmail.com>
2023-07-17 20:43:13 -0500

    [nvim] Recovering old eslint, prettier, and ChatGPT functionality

--- config/nvim/lua/user/init.lua
+++ config/nvim/lua/user/init.lua
@@ -1,13 +1,40 @@
 return {
-  lsp = {
-    servers = {
-      "clojure_lsp",
-    },
-  },
   options = {
     opt = {
+      history = 1000,
       number = false,
       relativenumber = false,
+    },
+  },
+  lsp = {
+    formatting = {
+      format_on_save = false,
+      filter = function(client)
+        -- Use null-ls only for typescript
+        if vim.bo.filetype == "typescript" then
+          return client.name == "null-ls"
+        end
+
+        return true
+      end 
+    },
+    servers = {
+      "clojure_lsp",
+      "tsserver"
+    },
+  },
+  mappings = {
+    n = {
+      ["<leader><leader>"] = {
+        "<cmd>ChatGPT<cr>",
+        desc = "ChatGPT",
+      },
+    },
+    v = {
+      ["<leader><leader>"] = {
+        "<cmd>ChatGPTRun follow_instructions<cr>",
+        desc = "ChatGPT follow instructions",
+      },
     },
   },
 }
--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -1,45 +1,77 @@
 return {
   {
     "airblade/vim-rooter",
-    lazy = false,
+    event = "VeryLazy",
     init = function() 
       vim.g.rooter_silent_chdir = 1
     end,
   },
   {
     "clojure-vim/clojure.vim",
-    lazy = false,
-    init = function()
-      vim.g.clojure_align_subforms = 1
-      vim.g.clojure_fuzzy_indent_patterns = {
-        "^comment$", 
-        "^def", 
-        "^do", 
-        "^future$", 
-        "^let", 
-        "^tests", 
-        "^with"
-      }
-    end,
+    event = "VeryLazy",
   },
   {
     "github/copilot.vim",
-    lazy = false,
+    event = "VeryLazy",
   },
   {
     "guns/vim-sexp",
-    lazy = false,
+    event = "VeryLazy",
   },
   {
     "hrsh7th/nvim-cmp",
     opts = function (_, opts)
       local cmp = require "cmp"
 
+      -- Avoid interfering with copilot
       opts.mapping['<Tab>'] = cmp.config.disable
       opts.mapping['<S-Tab>'] = cmp.config.disable
 
       return opts
     end,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        actions_paths = {
+          "~/.config/nvim/lua/user/actions.json"
+        }
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint_d",
+        "prettierd",
+      },
+      config = function(_, opts)
+        local mason_null_ls = require "mason-null-ls"
+        local null_ls = require "null-ls"
+        mason_null_ls.setup(opts)
+        mason_null_ls.setup {
+          eslint_d = function()
+            null_ls.register(null_ls.builtins.diagnostics.eslint_d.with {
+              extra_args = { "--cache" },
+              filetypes = { "javascript", "typescript" },
+            })
+          end,
+          prettierd = function()
+            null_ls.register(null_ls.builtins.formatting.prettierd.with {
+              extra_filetypes = { "json", "markdown" } 
+            })
+          end,
+        }
+      end,
+    },
   },
   {
     "lukas-reineke/indent-blankline.nvim",
