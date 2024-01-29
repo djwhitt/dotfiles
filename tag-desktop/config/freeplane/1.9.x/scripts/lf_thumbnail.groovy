dbb9ba791186352080122644bd9c26b355caaeea
David Whittington <djwhitt@gmail.com>
2024-01-28 20:08:57 -0600

    Work on freeplan thumbnail script

--- tag-desktop/config/freeplane/1.9.x/scripts/lf_thumbnail.groovy
+++ tag-desktop/config/freeplane/1.9.x/scripts/lf_thumbnail.groovy
@@ -1,3 +1,3 @@
 boolean overwriteExistingFile = true
-def filename = '/tmp/' + node.map.file.name.replaceFirst('.mm$', '.jpg')
+def filename = '/home/djwhitt/.cache/lf/thumbnail.tmp.freeplane.' + node.map.file.name.replaceFirst('.mm$', '.jpg')
 c.export(node.map, new File(filename), 'Compressed image (JPEG) (.jpg)', overwriteExistingFile)
