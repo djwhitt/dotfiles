94abf3b98b77b34c3e69382560a8220b80961758
David Whittington <djwhitt@gmail.com>
2024-11-17 12:28:39 -0600

    Add descriptions to d and bb fish completions

--- config/fish/completions/bb.fish
+++ config/fish/completions/bb.fish
@@ -1,5 +1,5 @@
 function __bb_complete_tasks
-  bb tasks |tail -n +3 |cut -f1 -d ' ' 
+  bb tasks |tail -n +3 |awk '{printf "%s\t%s\n", $1, substr($0, index($0,$2))}'
 end
 
 complete -c bb -a "(__bb_complete_tasks)" -d 'tasks'
--- config/fish/completions/d.fish
+++ config/fish/completions/d.fish
@@ -1,5 +1,5 @@
 function __d_complete_tasks
-  d tasks |tail -n +3 |cut -f1 -d ' ' 
+  d tasks |tail -n +3 |awk '{printf "%s\t%s\n", $1, substr($0, index($0,$2))}'
 end
 
-complete -f -c d -a "(__d_complete_tasks)" -d 'tasks'
+complete -f -c d -a "(__d_complete_tasks)"
