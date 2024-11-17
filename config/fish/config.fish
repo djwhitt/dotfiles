bb6fd3685b5545987b2b011e6b91834ccbbe7e4d
David Whittington <djwhitt@gmail.com>
2024-11-17 14:43:16 -0600

    Add some quick editing shortcuts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,11 @@
 alias tl="tmuxp load"
 alias ls="ls --hyperlink=auto --color=auto"
 
+# Quick editing shortcuts
+alias ef="e ~/.config/fish/config.fish"
+alias et="e ~/.local/src/clj/djwhitt/tasks.clj"
+alias ev="e ~/.config/nvim/lua/plugins/astrocore.lua"
+
 if status is-interactive
   atuin init fish | source
 end
