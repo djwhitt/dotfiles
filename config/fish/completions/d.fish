c876850c7d17018a6069bd5536019395679f4414
David Whittington <djwhitt@gmail.com>
2025-03-31 19:53:39 -0500

    Adjust completions for just version of d script

--- config/fish/completions/d.fish
+++ config/fish/completions/d.fish
@@ -1,5 +1,1 @@
-function __d_complete_tasks
-  d tasks |tail -n +3 |awk '{printf "%s\t%s\n", $1, substr($0, index($0,$2))}'
-end
-
-complete -f -c d -a "(__d_complete_tasks)"
+complete -f -c d -a (just --summary)
