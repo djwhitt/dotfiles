66c3098b0c31be86538b0b5abe4b3cbd835f9def
David Whittington <djwhitt@gmail.com>
2026-05-05 15:44:08 -0500

    Remove the d wrapper for ~/.user.justfile
    Not using it anymore — j (just) is sufficient.

--- config/fish/completions/d.fish
+++
@@ -1,7 +0,0 @@
-function __d_complete_tasks
-    if test -f "$HOME/.user.justfile"
-        just --summary --justfile "$HOME/.user.justfile" 2>/dev/null | string split " "
-    end
-end
-
-complete -f -c d -a "(__d_complete_tasks)"
