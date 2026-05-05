7f1633f0ac656970b89e68213efec96e12466415
David Whittington <djwhitt@gmail.com>
2026-05-05 15:36:43 -0500

    Add ~/.local/bin to fish PATH
    rcm symlinks scripts from local/bin/ into ~/.local/bin, so this is where
    my user scripts live. Renaming the file from nix-paths.fish to paths.fish
    since it's no longer just nix-managed dirs.

--- config/fish/conf.d/nix-paths.fish
+++
@@ -1,5 +0,0 @@
-for dir in /run/current-system/sw/bin /etc/profiles/per-user/$USER/bin /opt/homebrew/bin /opt/homebrew/sbin
-    if test -d $dir
-        fish_add_path --prepend --global $dir
-    end
-end
